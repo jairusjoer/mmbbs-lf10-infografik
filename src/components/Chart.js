6f31cd589cc6bf239adeddb62e55e2f0228fc349
Jairus Joer・Macbook Pro <jairus.joer@neuwaerts.de>
2020-12-08 12:23:38 +0100

    Rewrite API call

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import '../assets/css/components/Chart.scss';
 
@@ -7,73 +7,54 @@ var apiStr = 'https://sandbox.iexapis.com/stable';
 apiStr += '/stock/IBM/chart/1y';
 apiStr += '?token=Tsk_bfcba392ab76443fb243298b50f4ff44';
 
-class Chart extends Component {
-    constructor() {
-        super();
+const Chart = () => {
+    const [error, setError] = useState(false);
+    const [data, setData] = useState({});
 
-        this.state = {
-            data: [],
-            isLoading: false,
-            error: null,
-        };
+    // https://medium.com/better-programming/how-to-fetch-data-from-an-api-with-react-hooks-9e7202b8afcd
+    async function fetchData() {
+        const response = await fetch(apiStr);
+        response
+            .json()
+            .then((response) => setData(response))
+            .catch((error) => setError(error));
     }
-    // Stage Management
-    componentDidMount() {
-        this.setState({ isLoading: true });
 
-        fetch(apiStr)
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-            })
-            .then((data) => this.setState({ data: data, isLoading: false }))
-            .catch((error) => this.setState({ error, isLoading: false }));
-    }
-    // Render Function
-    render() {
-        const { data, isLoading } = this.state;
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-        if (isLoading) {
+    // Custom Tooltip Component
+    const TooltipContent = ({ active, payload }) => {
+        if (active) {
             return (
-                <div className='chart'>
-                    <p>Loading ...</p>
+                <div className='chart-tooltip'>
+                    {payload.map((payload) => {
+                        return (
+                            <p>
+                                <span>{payload.name}</span> {payload.value}
+                            </p>
+                        );
+                    })}
                 </div>
             );
         }
-        // Custom Tooltip Component
-        const TooltipContent = ({ active, payload }) => {
-            if (active) {
-                return (
-                    <div className='chart-tooltip'>
-                        {payload.map((payload) => {
-                            return (
-                                <p>
-                                    <span>{payload.name}</span> {payload.value}
-                                </p>
-                            );
-                        })}
-                    </div>
-                );
-            }
-            return null;
-        };
-        // Rechart Component
-        return (
-            <div className='chart'>
-                <ResponsiveContainer>
-                    <LineChart data={data}>
-                        <XAxis dataKey='date' />
-                        <YAxis dataKey='high' yAxisId='left' />
-                        <Tooltip content={<TooltipContent />} />
-                        <Legend />
-                        <Line yAxisId='left' type='monotone' dot={false} dataKey='high' stroke='#8884d8' />
-                    </LineChart>
-                </ResponsiveContainer>
-            </div>
-        );
-    }
-}
-/*
- */
+        return null;
+    };
+    // Rechart Component
+    return (
+        <div className='chart'>
+            <ResponsiveContainer>
+                <LineChart data={data}>
+                    <XAxis dataKey='date' />
+                    <YAxis dataKey='high' yAxisId='left' />
+                    <Tooltip content={<TooltipContent />} />
+                    <Legend />
+                    <Line yAxisId='left' type='monotone' dot={false} dataKey='high' stroke='#8884d8' />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    );
+};
+
 export default Chart;
