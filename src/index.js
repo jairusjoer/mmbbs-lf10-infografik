dae5188851e8091d2b61db64a413abdb7f0fa8ee
Jairus Joer・Macbook Pro <jairus.joer@neuwaerts.de>
2021-02-09 23:31:01 +0100

    Move CSS to inline

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
-import './assets/css/global.scss';
+//import './assets/css/global.scss';
 
 ReactDOM.render(
   <BrowserRouter>
